d1e47bb352601c68cefea265cb538fb22f8ff8bf
Dylan <dwsampson5290@gmail.com>
2017-12-09 10:49:56 -0600

    revert to Cameron's solution to resize pizzas

--- views/js/main.js
+++ views/js/main.js
@@ -434,10 +434,10 @@ var resizePizzas = function(size) {
           console.log("bug in sizeSwitcher");
       }
 
-    //var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
+    var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
 
     for (var i = 0; i < randomPizzas.length; i++) {
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+      randomPizzas[i].style.width = newWidth + "%";
     }
   }
 

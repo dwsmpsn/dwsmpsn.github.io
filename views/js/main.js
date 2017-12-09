91d7897189f7b8a42b52c26b66a181fa168a79ff
Dylan <dwsampson5290@gmail.com>
2017-12-09 10:36:57 -0600

    added breaks to changePizzaSizes

--- views/js/main.js
+++ views/js/main.js
@@ -426,10 +426,13 @@ var resizePizzas = function(size) {
     switch(size) {
         case "1":
           return 0.25;
+          break;
         case "2":
           return 0.3333;
+          break;
         case "3":
           return 0.5;
+          break;
         default:
           console.log("bug in sizeSwitcher");
       }
